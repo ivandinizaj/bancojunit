921a5c2261369edf4408ffe990a64806152653f8
ivand <ivandinizaj@gmail.com>
2020-11-02 19:01:21 -0300

    init

---
+++ src/main/java/com/junit/IdadeNaoPermitidaException.java
@@ -0,0 +1,20 @@
+package com.junit;
+
+/**
+ * Exce��o a ser lan�ada quando a idade de um poss�vel novo cliente n�o for aceita.
+ * 
+ * @author Gustavo Farias
+ */
+public class IdadeNaoPermitidaException extends Exception {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+	public static String MSG_IDADE_INVALIDA = "A idade do cliente precisa estar entre 18 e 65 anos.";
+	
+	public IdadeNaoPermitidaException(String msg) {
+		super(msg);
+	}
+
+}
